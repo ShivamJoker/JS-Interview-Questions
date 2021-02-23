82f02c44c7947aece52f2246fc0ba25708cafb47
shivamjoker <singhshivamkr@gmail.com>
2021-02-23 18:56:32 +0530

    async sync

---
+++ async-sync.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+console.log('Hello brother');
+
+const data = fs.readFileSync('check-palindrome.js');
+console.log('Reading done', data);
+
+// setTimeout(() => {
+//   console.log('hello sister');
+// }, 0);
+
+console.log('Hello bitch');
