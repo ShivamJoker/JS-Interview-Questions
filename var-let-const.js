26106de516bb95fb05217bbf4aa26988a1ce131c
shivamjoker <singhshivamkr@gmail.com>
2021-03-07 17:35:03 +0530

    added let vs const vs var

---
+++ var-let-const.js
@@ -0,0 +1,32 @@
+var name = "Padhega India";
+
+let school = "Choti safed topi";
+
+var name = "Chintu";
+
+// let school = "medium safed topi";
+school = "badi safed topi";
+
+const city = "surat";
+
+// city = "bombay";
+// console.log(city);
+
+// console.log(age);
+var age = 6;
+
+// console.log(gf);
+let gf = "chinti";
+
+if (gf === "chinti") {
+  var chintiNum = 797968689690;
+  let chituNum = 121223213123;
+}
+
+const getChinti = () => {
+  var chintiAdd = "Kachrbag road 201";
+};
+
+console.log(chintiAdd);
+console.log(chintiNum);
+console.log(chituNum);
